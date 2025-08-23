dc484eaa63af2bd4047d70b24afde721c673c10d
Bers1982 <agdarus@mail.ru>
2025-08-23 20:09:47 +0300

    новый апдейт

--- Rifoms.Web/wwwroot/js/site.js
+++ Rifoms.Web/wwwroot/js/site.js
@@ -22,53 +22,6 @@ function fnCenterDiv() {
 }
 
 /**
- * ЗАГРУЗКА JSON-ДАННЫХ ПО ID ФОРМЫ
- * @param {any} formID
- */
-function fnLoadJSON(formID) {
-    $("#" + formID).submit(function (e) {
-        e.preventDefault();
-        try {
-            var xhr = new XMLHttpRequest();
-
-            //xhr.open('GET', '/api/polis/loadjson/3', true);
-            xhr.open('GET', 'http://185.35.130.36:5000/api/polis/loadjson/3', true);
-
-            //xhr.open('POST', 'http://192.168.1.200:5000/api/polis/loadjson', true);
-
-            xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
-            xhr.setRequestHeader('Content-type', 'application/json');
-            //xhr.setRequestHeader('Content-type', 'multipart/form-data');
-
-            xhr.onreadystatechange = () => {
-                if (xhr.status == 200 && xhr.readyState == 4) {
-                    console.log(`Правильный запрос`);
-                    var response = JSON.parse(xhr.responseText);
-                    $(`#${formID}_result`).addClass('btn-info');
-
-                    //Если вовзращаемый JSON ответ от контроллера не содержит параметра result
-                    $(`#${formID}_result`).html(response);
-
-                    //Если вовзращаемый JSON ответ от контроллера содержит параметра result
-                    //$(`#${formID}_result`).html(response.result);
-                }
-                else
-                    console.log(`Неправильный запрос`);
-            }
-            //Для POST запросов
-            //xhr.send();
-
-            //Для GET запросов
-            xhr.send(null);
-            return false;
-        }
-        catch (ex) {
-            console.log(ex);
-        }
-    });
-}
-
-/**
  * ПОИСК ПОЛИСОВ ПО ID ФОРМЫ
  * @param {any} formID
  */
@@ -129,10 +82,11 @@ function fnFindPolis(formID) {
                         else
                             console.log(`Неправильный запрос`);
 
-                    //if (xhr.readyState == 4 && xhr.status == 200) {
-                    //    alert(xhr.responseText);
-                    //}
-                    }, 1000);                   
+                        //if (xhr.readyState == 4 && xhr.status == 200) {
+                        //    alert(xhr.responseText);
+                        //}
+                    }, 500);
+                        
                 }
                 //Для POST запросов
                 xhr.send(JSON.stringify(zapros));
@@ -148,59 +102,6 @@ function fnFindPolis(formID) {
         //else {
         //    alert('Neudacha');
         //}
-    });
-}
-
-function fnFindPolis1(formID) {
-    $("#" + formID).submit(function (e) {
-        e.preventDefault();
-        var formData = new FormData(document.getElementById(e.target.id));
-
-        if (fnValidate(formID)) {
-            try {
-                var data = {
-                    FormID: `${formID}`,
-                    FAM: formData.get('FAM'),
-                    IM: formData.get('IM'),
-                    OT: formData.get('OT'),
-                    DR: formData.get('DR'),
-                    ENP: formData.get('ENP'),
-                    NPOL: formData.get('NPOL'),
-                    SPOL: formData.get('SPOL')
-                }
-                var xhr = new XMLHttpRequest();
-                var params = `FormID=${formID}&FAM=${data.FAM}&IM=${data.IM}&OT=${data.OT}&DR=${data.DR}&ENP=${data.ENP}&NPOL=${data.NPOL}&SPOL=${data.SPOL}`;
-                //xhr.open('GET', 'http://185.35.130.36:5000/api/polis/getpolis2?' + params, true);
-                //xhr.open('POST', '/api/polis/getpolis', true);
-                xhr.open('GET', '/api/polis/getpolis2?' + params, true);
-                //xhr.open('POST', 'http://185.35.130.36:5000/api/polis/getpolis', true);
-                xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val());
-                xhr.setRequestHeader('Content-type', 'application/json');
-
-                xhr.onreadystatechange = () => {
-                    if (xhr.status == 200 && xhr.readyState == 4) {
-                        console.log(`Правильный запрос`);
-                        var response = JSON.parse(xhr.responseText);
-                        $(`#${formID}_result`).addClass('btn-info');
-                        $(`#${formID}_result`).html(response.result);
-                    }
-                    else
-                        console.log(`Неправильный запрос`);
-
-                    //if (xhr.readyState == 4 && xhr.status == 200) {
-                    //    alert(xhr.responseText);
-                    //}
-                }
-
-                //xhr.send(JSON.stringify(data));
-                //Для GET запросов
-
-                xhr.send(null);
-                return false;
-            } catch (ex) {
-                console.log(ex)
-            }
-        }
     });
 }
 
