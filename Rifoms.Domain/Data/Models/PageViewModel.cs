1a349c0accc97a3c8eea4ea3ee979016f22242be
Bers1982 <agdarus@mail.ru>
2025-09-19 16:16:59 +0300

    new

--- Rifoms.Domain/Data/Models/PageViewModel.cs
+++ Rifoms.Domain/Data/Models/PageViewModel.cs
@@ -5,13 +5,30 @@ using System;
 namespace Rifoms.Domain.Data.Models
 {
     /// <summary>
-    /// 
+    /// PageViewModel, класс который будет содержать всю информацию о пагинации   
     /// </summary>
     public class PageViewModel: BaseModel
     {
+        /// <summary>
+        /// Номер текущей страницы в свойстве PageNumber
+        /// </summary>
         public int PageNumber { get; private set; }
+
+        /// <summary>
+        /// Общее количество страниц в свойстве TotalPages
+        /// </summary>
         public int TotalPages { get; private set; }
+
+        /// <summary>
+        /// ПРизнак сущестования ДО текущей страницы,
+        /// есть ли еще какие-нибудь страницы
+        /// </summary>
         public bool HasPreviousPage => PageNumber > 1;
+
+        /// <summary>
+        /// ПРизнак сущестования ПОСЛЕ текущей страницы,
+        /// есть ли еще какие-нибудь страницы
+        /// </summary>
         public bool HasNextPage => PageNumber < TotalPages;
         public PageViewModel(int count,int pageNumber,int pageSize)
         {
